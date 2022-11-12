443a4d53d49d581fbd0378ed67ddde3fc7b9aaec
Makktu <johnmcna@gmail.com>
2022-11-12 19:05:01 +0000

    Added insert & delete methods

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-class BSTNode {
+class Node {
     constructor(data, left = null, right = null) {
         this.data = data;
         this.left = left;
@@ -8,34 +8,82 @@ class BSTNode {
     }
 }
 
-class BinarySearchTree {
+class BinaryTree {
     constructor(arr) {
-        this.root = this.setUpTree(arr);
-        console.log(this.root);
+        this.root = this.buildTree(arr);
+        console.log(this);
     }
 
-    setUpTree(arr) {
-        // ? remove duplicates/sort etc., eventually
-        // ? for testing, array arrives ready to go
-        return this.treeBuilder(arr);
+    sortArr(arr) {
+        // sort arr
+        arr.sort(function (a, b) {
+            return a - b;
+        });
+
+        // remove duplicates
+        arr = [...new Set(arr)];
+        console.log(arr);
+
+        return arr;
     }
 
-    treeBuilder(arr) {
+    buildTree(arr) {
         if (arr.length == 0) return null;
+        // do the sorting
+        // and removal of duplicates here
+        this.sortArr(arr);
         let halfIndex = Math.floor(arr.length / 2);
-        // creates a rootnode with the correct values
-        // in left and right positions
-        // because the array is sorted and this
-        // is guaranteed
-        const rootNode = new BSTNode(
-            arr[halfIndex],
-            this.treeBuilder(arr.slice(0, halfIndex)),
-            this.treeBuilder(arr.slice(halfIndex + 1))
-        );
-        return rootNode;
+        console.log(halfIndex);
+        let node = new Node(arr[halfIndex]);
+        node.left = this.buildTree(arr.slice(0, halfIndex));
+        node.right = this.buildTree(arr.slice(halfIndex + 1));
+        return node;
+    }
+
+    insert(value) {
+        arr.push(value);
+        arr = this.sortArr(arr);
+        this.root = this.buildTree(arr);
+    }
+
+    delete(value) {
+        // 1) find & delete the value in the original array
+        // 2) rebuild the tree
+        arr.forEach((num) => {
+            if (num == value) {
+                let getRid = arr.indexOf(num);
+                arr.splice(getRid, 1);
+            }
+        });
+        this.sortArr(arr);
+        this.root = this.buildTree(arr);
+        // * advantage of this sample case is that all the values in the array are unique
     }
 }
 
-const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node.right !== null) {
+        prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+        prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+};
 
-let newTree = new BinarySearchTree(arr);
+let arr = [1, 2, 3, 4, 5, 1, 2, 3, 4, 5];
+
+let newTree = new BinaryTree(arr);
+
+prettyPrint(newTree.root);
+console.log(newTree.root);
+
+newTree.insert(77);
+prettyPrint(newTree.root);
+newTree.insert(5);
+prettyPrint(newTree.root);
+newTree.delete(77);
+prettyPrint(newTree.root);
+
+// 1) get the root
+// 2) recursively call method to assign other elements of array
